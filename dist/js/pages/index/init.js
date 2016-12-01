ac31fa888a84ee02aedb1b57901f519e972a54e7
Your Name <liqin@300.cn>
2016-12-01 18:00:34 +0800

    优化登录页

--- dist/js/pages/index/init.js
+++ dist/js/pages/index/init.js
@@ -13,7 +13,7 @@ var browser = {
     language: (navigator.browserLanguage || navigator.language).toLowerCase()
 };
 var ios_c=browser.versions.iPhone;
-//  功能：获取cookies函数  参数：name，cookie名字
+//  功能：获取和设置cookies函数  参数：name，cookie名字
 var getCookie = function getCookie(name){
     var arr = document.cookie.match(new RegExp("(^| )"+name+"=([^;]*)(;|$)"));
     if(arr != null){
@@ -25,4 +25,10 @@ var getCookie = function getCookie(name){
     }else{
         return null;
     }
+};
+var SetCookie=function(name,value){
+    var Days = 30;   //cookie 将被保存一月
+    var exp  = new Date();  //获得当前时间
+    exp.setTime(exp.getTime() + Days*24*60*60*1000);  //换成毫秒
+    document.cookie = name + "="+ escape (value) + ";expires=" + exp.toGMTString();
 };
